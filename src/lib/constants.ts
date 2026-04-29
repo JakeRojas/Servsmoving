59a90400ca1acca5b13b3524538fd906f1e3bcb5
Jake Rojas <jakerojas2211@gmail.com>
2026-04-29 12:56:07 +0800

    11, added a .env file for securing confidential data

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const CONTACT_INFO = {
+    phone: import.meta.env.PUBLIC_PHONE_NUMBER || '0907-888-0557',
+    phoneRaw: import.meta.env.PUBLIC_PHONE_RAW || '09078880557',
+    email: import.meta.env.PUBLIC_CONTACT_EMAIL || 'jakeianrojas@gmail.com',
+    formSubmitUrl: import.meta.env.PUBLIC_FORMSUBMIT_URL || 'https://formsubmit.co/jakeianrojas@gmail.com',
+};
